bc5570e914abee690e58d7d5d5c759e8ae4c1b72
lzao7 <lizihao712@163.com>
2025-05-13 14:52:50 +0800

    Add files via upload

---
+++ DATA/FOLorenz_Example.m
@@ -0,0 +1,19 @@
+Y0range=[0.1,0.5]
+parameters=[10,28,8/3]
+orders=[0.9,0.9,0.9]
+TSim=0.025
+numICs=15000
+results = zeros(numICs*6, 3)
+test_results = zeros(6000, 3)
+for i = 1:numICs
+    Y0 = Y0range(1) + (Y0range(2) -Y0range(1)) * rand(1, 3)
+    [T, Y]=FOLorenz(parameters, orders, TSim, Y0)
+     results((i-1)*6+1:(i-1)*6+6, :) = Y(:, :);
+end
+csvwrite('FOLorenz_train_0.90.csv', results);
+for i = 1:1000
+    Y0 = Y0range(1) + (Y0range(2) -Y0range(1)) * rand(1, 3)
+    [T, Y]=FOLorenz(parameters, orders, TSim, Y0)
+     test_results((i-1)*6+1:(i-1)*6+6, :) = Y(:, :);
+end
+csvwrite('FOLorenz_test_0.90.csv', test_results);
---
+++ DATA/FOSpectrum.m
@@ -0,0 +1,49 @@
+function [T, Y]=FOSpectrum(parameters, orders, TSim, Y0)
+%
+% Numerical Solution of the Fractional-Order Chen's System
+%
+%   D^q1 x(t) = a(y(t)-x(t))
+%   D^q2 y(t) = dx(t) - x(t)z(t) + cy(t)
+%   D^q3 z(t) = x(t)y(t) - bz(t)
+%
+% function [T, Y] = FOChen(parameters, orders, TSim, Y0)
+%
+% Input:    parameters - model parameters [a, b, c, d]
+%           orders - derivatives orders [q1, q2, q3]
+%           TSim - simulation time (0 - TSim) in sec
+%           Y0 - initial conditions [Y0(1), Y0(2), Y0(3)]
+%
+% Output:   T - simulation time (0 : Tstep : TSim)
+%           Y - solution of the system (x=Y(1), y=Y(2), z=Y(3))
+%
+% Author:  (c) Ivo Petras (ivo.petras@tuke.sk), 2010.
+%
+% time step:
+h=0.005; 
+% number of calculated mesh points:
+n=round(TSim/h)+1;
+%orders of derivatives, respectively:
+q1=orders(1); q2=orders(2); 
+% constants of Chen's system:
+a=parameters(1); b=parameters(2); 
+
+% binomial coefficients calculation:
+cp1=1; cp2=1; 
+for j=1:n
+    c1(j)=(1-(1+q1)/j)*cp1;
+    c2(j)=(1-(1+q2)/j)*cp2;
+    cp1=c1(j); cp2=c2(j); 
+end
+% initial conditions setting:
+x(1)=Y0(1); y(1)=Y0(2); 
+% calculation of phase portraits /numerical solution/:
+for i=2:n
+    x(i)=(a*x(i-1))*h^q1 - memo(x, c1, i);
+    y(i)=b*(-x(i)*x(i)+y(i-1))*h^q2 - memo(y, c2, i);
+end
+for j=1:n
+    Y(j,1)=x(j);
+    Y(j,2)=y(j);
+end
+T=0:h:TSim;
+%
