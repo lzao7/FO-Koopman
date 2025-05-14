aa8aa4eacd2f0ba336d48e4bcbf6265199af630f
lzao7 <lizihao712@163.com>
2025-05-14 18:04:48 +0800

    Delete DATA directory

--- DATA/W_w.m
+++
@@ -1,8 +0,0 @@
-function [out]=W_w(a, b, in);
-if abs(in) < 1 
-   out = a;
-end
-%
-if abs(in) > 1 
-   out = b;
-end
--- DATA/memo.m
+++
@@ -1,8 +0,0 @@
-function [yo] = memo(r, c, k)
-%
-temp = 0;
-for j=1:k-1
-   temp = temp + c(j)*r(k-j);
-end
-yo = temp;
-%
